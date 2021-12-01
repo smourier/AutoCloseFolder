2e8e5002a813b2667650614ea6f4961db1c54f52
Simon Mourier <simon_mourier@hotmail.com>
2021-12-01 13:23:49 +0100

    Added Visual Studio 2022 support

--- AutoCloseFolder/source.extension.cs
+++ AutoCloseFolder/source.extension.cs
@@ -11,7 +11,7 @@ namespace AutoCloseFolder
         public const string Name = "Auto Close Folders";
         public const string Description = @"A Visual Studio Extension that automatically closes a folder when no document in that folder is open.";
         public const string Language = "en-US";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
         public const string Author = "Simon Mourier";
         public const string Tags = "solution explorer, cleanup, collapse";
     }
